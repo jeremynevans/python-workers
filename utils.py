71212d10871cd839ac41122e492fff4807626e56
Jeremy Evans <jeremy.n.evans@gmail.com>
2018-06-02 12:50:20 -0700

    2

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 import requests, os
 import redis
 
-redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
+redis_url = os.getenv('REDIS_URL', 'redis://localhost:6379')
 redis_conn = redis.from_url(redis_url)
 
 def greet(data: dict):
--- worker.py
+++ worker.py
@@ -4,7 +4,7 @@ from rq import Worker, Queue, Connection
 
 listen = ['high', 'default', 'low']
 
-redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
+redis_url = os.getenv('REDIS_URL', 'redis://localhost:6379')
 print('redis_url: ' + redis_url)
 
 redis_conn = redis.from_url(redis_url)
